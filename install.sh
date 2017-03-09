097e01fe458127d7dbe9d0cc4b84a76a7cdae855
Jaehong Seo <jaehong@ardunux.com>
2017-03-09 19:59:39 +0900

    add sudo, check ArchLinux and do not copy sv directory
    add sudo for running widish command as root.
    if os-arch is “Arch Linux”, do not copy sv directory to “/etc”
    directory.

--- install.sh
+++ install.sh
@@ -1,9 +1,15 @@
 #!/bin/sh -e
-install -D -m0644 awk/wscanparse.awk /var/lib/wifish/wscanparse.awk
-install -D -m0644 awk/wlistparse.awk /var/lib/wifish/wlistparse.awk
-install -D -m0644 awk/wscan2menu.awk /var/lib/wifish/wscan2menu.awk
-install -D -m0644 awk/iwparse.awk /var/lib/wifish/iwparse.awk
-install -D -m0755 wifish /usr/local/bin
+
+SUDO=""
+if [ $(id -u) -ne 0 ]; then
+        SUDO="sudo"
+fi
+
+$SUDO install -D -m0644 awk/wscanparse.awk /var/lib/wifish/wscanparse.awk
+$SUDO install -D -m0644 awk/wlistparse.awk /var/lib/wifish/wlistparse.awk
+$SUDO install -D -m0644 awk/wscan2menu.awk /var/lib/wifish/wscan2menu.awk
+$SUDO install -D -m0644 awk/iwparse.awk /var/lib/wifish/iwparse.awk
+$SUDO install -D -m0755 wifish /usr/local/bin
 
 # check it is Arch Linux
 _os_arch=""
@@ -15,10 +21,10 @@ fi
 if [ -z "$_os_arch" ]; then
 	if [ ! -d /etc/sv/wpa_supplicant ];then
 		echo "Installing /etc/sv/wpa_supplicant service"
-		install -D -d -m0755 /etc/sv/wpa_supplicant
-		install -D -m0644 sv/wpa_supplicant/conf /etc/sv/wpa_supplicant/conf
-		install -D -m0755 sv/wpa_supplicant/run /etc/sv/wpa_supplicant/run
-		install -D -m0755 sv/wpa_supplicant/log/run /etc/sv/wpa_supplicant/log/run
+		$SUDO install -D -d -m0755 /etc/sv/wpa_supplicant
+		$SUDO install -D -m0644 sv/wpa_supplicant/conf /etc/sv/wpa_supplicant/conf
+		$SUDO install -D -m0755 sv/wpa_supplicant/run /etc/sv/wpa_supplicant/run
+		$SUDO install -D -m0755 sv/wpa_supplicant/log/run /etc/sv/wpa_supplicant/log/run
 	fi
 fi
 echo ".. Wifish is installed .."
